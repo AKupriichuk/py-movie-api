9fca0edd52799b339d3a57e54989bf21d5704644
AKupriichuk <alexa05kup@gmail.com>
2026-02-16 22:34:20 +0200

    c3

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -3,21 +3,6 @@ from cinema.models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
-    title = serializers.SerializerMethodField()
-    description = serializers.SerializerMethodField()
-    duration = serializers.SerializerMethodField()
-
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
-        instance.duration = validated_data.get('duration', instance.duration)
-        instance.save()
-        return instance
-
     class Meta:
         model = Movie
-        fields = ('id', 'title', 'description', 'duration')
-
+        fields = ("id", "title", "description", "duration")
--- cinema/views.py
+++ cinema/views.py
@@ -1,18 +1,20 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Movie
-from .serializers import MovieSerializer
+
+from cinema.models import Movie
+from cinema.serializers import MovieSerializer
 
 
 @api_view(["GET", "POST"])
 def movie_list(request):
-   if request.method == "GET":
-       movie = Movie.objects.all()
-       serializer = MovieSerializer(movie, many=True)
-       return Response(serializer.data, status=200)
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-   if request.method == "POST":
+    if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
@@ -21,11 +23,7 @@ def movie_list(request):
 
 @api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
-
-    try:
-        movie = Movie.objects.get(pk=pk)
-    except Movie.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    movie = get_object_or_404(Movie, pk=pk)
 
     if request.method == "GET":
         serializer = MovieSerializer(movie)
